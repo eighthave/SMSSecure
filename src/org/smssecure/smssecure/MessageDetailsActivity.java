SamSprung/Signal-Android

add custom color to MessageDetailsActivity
Fixes #3560
Closes #3580

// FREEBIE

src/org/thoughtcrime/securesms/MessageDetailsActivity.java
@@ -18,7 +18,9 @@ package org.thoughtcrime.securesms;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
@@ -31,6 +33,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.thoughtcrime.securesms.color.MaterialColor;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.EncryptingSmsDatabase;
@@ -46,6 +49,7 @@ import org.thoughtcrime.securesms.util.DateUtils;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicTheme;
 import org.thoughtcrime.securesms.util.GroupUtil;
+import org.thoughtcrime.securesms.util.Util;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -58,13 +62,14 @@ import java.util.Locale;
 /**
  * @author Jake McGinty
  */
-public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity implements LoaderCallbacks<Cursor> {
+public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity implements LoaderCallbacks<Cursor>, Recipients.RecipientsModifiedListener {
   private final static String TAG = MessageDetailsActivity.class.getSimpleName();
 
-  public final static String MASTER_SECRET_EXTRA = "master_secret";
-  public final static String MESSAGE_ID_EXTRA    = "message_id";
-  public final static String IS_PUSH_GROUP_EXTRA = "is_push_group";
-  public final static String TYPE_EXTRA          = "type";
+  public final static String MASTER_SECRET_EXTRA  = "master_secret";
+  public final static String MESSAGE_ID_EXTRA     = "message_id";
+  public final static String IS_PUSH_GROUP_EXTRA  = "is_push_group";
+  public final static String TYPE_EXTRA           = "type";
+  public final static String RECIPIENTS_IDS_EXTRA = "recipients_ids";
 
   private MasterSecret     masterSecret;
   private boolean          isPushGroup;
@@ -94,8 +99,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     setContentView(R.layout.message_details_activity);
 
     initializeResources();
-
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    initializeActionBar();
     getSupportLoaderManager().initLoader(0, null, this);
   }
 
@@ -105,6 +109,33 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     dynamicTheme.onResume(this);
     dynamicLanguage.onResume(this);
     getSupportActionBar().setTitle(R.string.AndroidManifest__message_details);
+  }
+
+  private void initializeActionBar() {
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    Recipients recipients = RecipientFactory.getRecipientsForIds(this, getIntent().getLongArrayExtra(RECIPIENTS_IDS_EXTRA), true);
+    recipients.addListener(this);
+
+    setActionBarColor(recipients.getColor());
+  }
+
+  private void setActionBarColor(MaterialColor color) {
+    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color.toActionBarColor(this)));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      getWindow().setStatusBarColor(color.toStatusBarColor(this));
+    }
+  }
+
+  @Override
+  public void onModified(final Recipients recipients) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        setActionBarColor(recipients.getColor());
+      }
+    });
   }
 
   private void initializeResources() {
@@ -274,7 +305,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
                                       .getGroupMembers(GroupUtil.getDecodedId(groupId), false);
         } catch (IOException e) {
           Log.w(TAG, e);
-         recipients = RecipientFactory.getRecipientsFor(MessageDetailsActivity.this, new LinkedList<Recipient>(), false);
+          recipients = RecipientFactory.getRecipientsFor(MessageDetailsActivity.this, new LinkedList<Recipient>(), false);
         }
       }
 

eighthave/SMSSecure

Fixes https://github.com/WhisperSystems/TextSecure/issues/3560
Closes https://github.com/WhisperSystems/TextSecure/pull/3580
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/0ef56fb86d437a16ea9ed85317b4f1dffd937c29

src/org/smssecure/smssecure/MessageDetailsActivity.java
@@ -18,7 +18,9 @@ package org.smssecure.smssecure;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
@@ -31,6 +33,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.smssecure.smssecure.color.MaterialColor;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.database.EncryptingSmsDatabase;
@@ -47,6 +50,7 @@ import org.smssecure.smssecure.util.DynamicLanguage;
 import org.smssecure.smssecure.util.DynamicTheme;
 import org.smssecure.smssecure.util.GroupUtil;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
+import org.smssecure.smssecure.util.Util;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -59,12 +63,13 @@ import java.util.Locale;
 /**
  * @author Jake McGinty
  */
-public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity implements LoaderCallbacks<Cursor> {
+public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity implements LoaderCallbacks<Cursor>, Recipients.RecipientsModifiedListener {
   private final static String TAG = MessageDetailsActivity.class.getSimpleName();
 
-  public final static String MASTER_SECRET_EXTRA = "master_secret";
-  public final static String MESSAGE_ID_EXTRA    = "message_id";
-  public final static String TYPE_EXTRA          = "type";
+  public final static String MASTER_SECRET_EXTRA  = "master_secret";
+  public final static String MESSAGE_ID_EXTRA     = "message_id";
+  public final static String TYPE_EXTRA           = "type";
+  public final static String RECIPIENTS_IDS_EXTRA = "recipients_ids";
 
   private MasterSecret     masterSecret;
   private ConversationItem conversationItem;
@@ -93,8 +98,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     setContentView(R.layout.message_details_activity);
 
     initializeResources();
-
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    initializeActionBar();
     getSupportLoaderManager().initLoader(0, null, this);
   }
 
@@ -104,6 +108,33 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     dynamicTheme.onResume(this);
     dynamicLanguage.onResume(this);
     getSupportActionBar().setTitle(R.string.AndroidManifest__message_details);
+  }
+
+  private void initializeActionBar() {
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    Recipients recipients = RecipientFactory.getRecipientsForIds(this, getIntent().getLongArrayExtra(RECIPIENTS_IDS_EXTRA), true);
+    recipients.addListener(this);
+
+    setActionBarColor(recipients.getColor());
+  }
+
+  private void setActionBarColor(MaterialColor color) {
+    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color.toActionBarColor(this)));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      getWindow().setStatusBarColor(color.toStatusBarColor(this));
+    }
+  }
+
+  @Override
+  public void onModified(final Recipients recipients) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        setActionBarColor(recipients.getColor());
+      }
+    });
   }
 
   private void initializeResources() {
@@ -284,7 +315,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
                                       .getGroupMembers(GroupUtil.getDecodedId(groupId), false);
         } catch (IOException e) {
           Log.w(TAG, e);
-         recipients = RecipientFactory.getRecipientsFor(MessageDetailsActivity.this, new LinkedList<Recipient>(), false);
+          recipients = RecipientFactory.getRecipientsFor(MessageDetailsActivity.this, new LinkedList<Recipient>(), false);
         }
       }
 